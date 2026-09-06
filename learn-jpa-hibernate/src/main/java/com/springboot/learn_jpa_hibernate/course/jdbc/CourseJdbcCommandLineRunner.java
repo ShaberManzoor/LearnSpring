391df2bd46d75fe271a6ece1b6fad42ac0e78ea7
Shaber Manzoor <kshaber313@gmail.com>
2026-09-06 23:40:54 +0530

    Java Spring Data Jpa

--- learn-jpa-hibernate/src/main/java/com/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-hibernate/src/main/java/com/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -5,7 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.springboot.learn_jpa_hibernate.course.Course;
-import com.springboot.learn_jpa_hibernate.course.jpa.CouseJpaRepository;
+import com.springboot.learn_jpa_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
@@ -13,19 +13,24 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 //	@Autowired
 //	private CourseJdbcRepository repo;
 	
+//	@Autowired
+//	private CouseJpaRepository repo;
+	
 	@Autowired
-	private CouseJpaRepository repo;
+	private CourseSpringDataJpaRepository repo;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repo.insert(new Course(1, "Learn Spring", "in28mins"));
-		repo.insert(new Course(2, "Learn AWS", "in28mins"));
-		repo.insert(new Course(3, "Learn DevOps", "in28mins"));
+		repo.save(new Course(1, "Learn Spring", "in28mins"));
+		repo.save(new Course(2, "Learn AWS", "in28mins"));
+		repo.save(new Course(3, "Learn DevOps", "in28mins"));
 		
-		repo.deleteById(3);
+		repo.deleteById(3l);
 		
-		System.out.println(repo.findById(1));
-		System.out.println(repo.findById(2));
+		System.out.println(repo.findById(1l));
+		System.out.println(repo.findById(2l));
+		
+		System.out.println(repo.findByAuthor("in28mins"));
 	}
 }
