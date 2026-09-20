3fe3034ad3c1afd93763dd34a72143d4eafe5167
Shaber Manzoor <kshaber313@gmail.com>
2026-09-20 23:24:41 +0530

    Throwing error not found

--- restful-web-services/src/main/java/com/learn_springboot/rest/webservices/restful_web_services/user/UserController.java
+++ restful-web-services/src/main/java/com/learn_springboot/rest/webservices/restful_web_services/user/UserController.java
@@ -27,7 +27,11 @@ public class UserController {
 	
 	@GetMapping(path="/users/{id}")
 	public User getUser(@PathVariable int id) {
-		return userService.findUser(id);
+		User user = userService.findUser(id);
+		
+		if(user==null) throw new UserNotFoundException("id: " + id);
+		
+		return user;
 	}
 	
 	@PostMapping(path="/users")
